f79b04585b68383cdab4e96f3a22e18d31e9b8e1
Vincennes Wong <chesed.sephiroth@gmail.com>
2022-02-13 01:45:41 +0800

    change back to 3000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const hbs = require('hbs')
 const routes = require('./routes/routes')
 const path = require('path')
 const app = express()
-const PORT = 80
+const PORT = 3000
 
 app.set('view engine', hbs)
 app.use('/', routes)
